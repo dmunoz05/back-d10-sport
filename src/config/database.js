cf667f79a3fe95dcfe09d9407f1d042b60d5fd4e
Daniel Muñoz <daniel20025febrero29@gmail.com>
2024-11-04 17:40:36 -0500

    Database and yml

--- src/config/database.js
+++ src/config/database.js
@@ -3,12 +3,13 @@ import mysql2 from 'mysql2';
 // Connects to the Mysql database
 const connectionMysql = async () => {
   try {
-    const connection = mysql2.createConnection({
+    const connection = await mysql2.createConnection({
       host: process.env.MYSQL_HOST || '',
       user: process.env.MYSQL_USER || '',
+      port: process.env.MYSQL_PORT || '',
       password: process.env.MYSQL_PASSWORD || '',
       database: process.env.MYSQL_DATABASE || ''
-    });
+    }).promise();
     return connection;
   } catch (error) {
     console.error("Error connecting to MySQL:", error);

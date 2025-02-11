9bcf257457e442ea7d2ccf2c5c6032f103dc6b5f
Deymer Ospina <deymer.ospina@gmail.com>
2025-02-11 04:34:04 -0500

    New query select in function getClassComments

--- src/controllers/academy/class.controller.js
+++ src/controllers/academy/class.controller.js
@@ -24,29 +24,24 @@ export const getClassContent = async (req, res) => {
 }
 
 export const getClassComments = async (req, res) => {
-    const { id_course } = req.query;
+    const { id_class } = req.query; // Usar id_class en lugar de id_course
     const conn = await getConnection();
     const db = variablesDB.academy;
-    const select = await conn.query(`SELECT cm.comment comentario, CONCAT(au.first_names, ' ', au.last_names) nombre, lu.email correo
-    FROM ${db}.class_course cc
-    INNER JOIN ${db}.comments_couser cm ON cm.id_class = cc.id
-    LEFT JOIN ${db}.login_users lu ON lu.id_user = cm.id_user
-    RIGHT JOIN ${db}.athletes_user au ON au.id = lu.id_athlete
-    WHERE cc.id_course = ?
-    UNION ALL
-    SELECT cm.comment comentario, CONCAT(cu.first_names, ' ', cu.last_names) nombre, lu.email correo
-    FROM ${db}.class_course cc
-    INNER JOIN ${db}.comments_couser cm ON cm.id_class = cc.id
-    LEFT JOIN ${db}.login_users lu ON lu.id_user = cm.id_user
-    INNER JOIN ${db}.coach_user cu ON cu.id = lu.id_coach
-    WHERE cc.id_course = ?
-    UNION ALL
-    SELECT cm.comment comentario, cu.name_club nombre, lu.email correo
-    FROM ${db}.class_course cc
-    INNER JOIN ${db}.comments_couser cm ON cm.id_class = cc.id
-    LEFT JOIN ${db}.login_users lu ON lu.id_user = cm.id_user
-    INNER JOIN ${db}.club_user cu ON cu.id = lu.id_club
-    WHERE cc.id_course = ?`, [id_course, id_course, id_course]);
+
+    const select = await conn.query(
+        `SELECT cm.comment comentario, 
+                COALESCE(CONCAT(au.first_names, ' ', au.last_names), cu.name_club, 'Usuario Desconocido') AS nombre, 
+                lu.email correo
+        FROM ${db}.comments_couser cm
+        LEFT JOIN ${db}.login_users lu ON lu.id_user = cm.id_user
+        LEFT JOIN ${db}.athletes_user au ON au.id = lu.id_athlete
+        LEFT JOIN ${db}.coach_user ca ON ca.id = lu.id_coach
+        LEFT JOIN ${db}.club_user cu ON cu.id = lu.id_club
+        WHERE cm.id_class = ?`,
+        [id_class]
+    );
+
     if (!select) return res.json(responseQueries.error({ message: "Error connecting" }));
     return res.json(responseQueries.success({ data: select[0] }));
-}
+};
+

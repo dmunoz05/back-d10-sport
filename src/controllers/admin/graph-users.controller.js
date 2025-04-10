7da39a8c83ff0ac92bd266bca33cee07317bb484
Daniel Muñoz <daniel20025febrero29@gmail.com>
2025-04-09 22:54:04 -0500

    Changes for show error

--- src/controllers/admin/graph-users.controller.js
+++ src/controllers/admin/graph-users.controller.js
@@ -14,9 +14,14 @@ export const getUsersFromClub = async (req, res) => {
 export const getAllUsers = async (req, res) => {
     const conn = await getConnection();
     const db = variablesDB.academy;
-    const select = await conn.query(`SELECT id_user, first_names, last_names, gender, city, contact, mail, social_networks FROM ${db}.athlete
-UNION
-SELECT id_user, first_names, last_names, gender, city, contact, mail, social_networks FROM ${db}.coach`, []);
+    const select = await conn.query(`SELECT id_user, first_names, last_names, gender, city, contact, mail, social_networks 
+    FROM ${db}.athlete
+    UNION
+    SELECT id_user, first_names, last_names, gender, city, contact, mail, social_networks
+    FROM ${db}.coach
+    UNION
+    SELECT id_user, president AS first_names, name_club AS last_names, NULL AS gender, city, contact, mail, social_networks 
+    FROM ${db}.club;`, []);
     if (!select) return res.json(responseQueries.error({ message: "Error obteniendo las clases" }));
     return res.json(responseQueries.success({ data: select[0] }));
 }
